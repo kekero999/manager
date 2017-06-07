64c38fd6ede2b9ccd5102efb3e3e1b4112982363
kekero999 <kekero999@gmail.com>
2017-06-07 18:23:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-sssssssssss
+<?php
+  $var2 = $_GET['State'];   //Tên biến State
+  $fileContent = $var2;
+  file_put_contents("data.html", $fileContent); //Lưu giá trị của biến State vào file data.html
+  echo 'sssssssssssddddd';
+?>
